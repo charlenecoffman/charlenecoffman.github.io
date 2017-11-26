8b8efc87c734c299aeebc04c2e24a5347d9854d1
Charlene Coffman <Charlene.Coffman@intlfcstone.com>
2017-11-25 19:35:03 -0600

    Fixed the mobile menu!

--- layout/scripts/jquery.shared.js
+++ layout/scripts/jquery.shared.js
@@ -1,19 +1,35 @@
-$(document).ready(function() { 
-	$("#header_placeholder").load("/includes/header.html");
-	$("#footer_placeholder").load("/includes/footer.html");
-	$("#copyright_placeholder").load("/includes/copyright.html");
-	$("#topnav_placeholder").load("/includes/topmedianav.html");
+
+$("#header_placeholder").load("/includes/header.html");
+$("#footer_placeholder").load("/includes/footer.html");
+$("#copyright_placeholder").load("/includes/copyright.html");
+$("#topnav_placeholder").load("/includes/topmedianav.html");
+
+$.ajax({
+		url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
+		dataType: "json",
+		jsonpCallback: "xkcddata",
+		success: function(data) {
+					$("#xkcd_comic").attr({
+							src: data.img,
+							title: data.alt,
+							alt: data.title
+					});
+				 }
+	  });
+		  
+function ShowPoem() {
 	
-	$.ajax({
-			url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
-			dataType: "json",
-			jsonpCallback: "xkcddata",
-			success: function(data) {
-						$("#xkcd_comic").attr({
-								src: data.img,
-								title: data.alt,
-								alt: data.title
-						});
-					 }
-		  });
-});
+	httpGetAsync('http://poetrydb.org/author,title/Shakespeare;From%20fairest');
+}
+	
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+	
