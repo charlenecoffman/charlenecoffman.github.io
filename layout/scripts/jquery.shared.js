a12e489e40e309893b8f6c260d445ddec56eb60c
Charlene Coffman <Charlene.Coffman@intlfcstone.com>
2017-11-26 13:11:32 -0600

    Updating some things - randomizing poem better

--- layout/scripts/jquery.shared.js
+++ layout/scripts/jquery.shared.js
@@ -47,7 +47,7 @@ function httpGetAsync(theUrl, callback)
 
 function GetRandomWord() {
 	
-	var arrayOfRandomWords = new Array('grass', 'love', 'stars', 'eyes', 'dance');
+	var arrayOfRandomWords = new Array('grass', 'love', 'stars', 'eyes', 'dance', 'lovers', 'child', 'sing', 'song', 'mouth');
 	var randomnumber = Math.floor(Math.random() * arrayOfRandomWords.length)
 	
 	return arrayOfRandomWords[randomnumber];
