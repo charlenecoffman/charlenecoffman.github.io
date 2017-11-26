d00684c2a34272bf8dab329b7364852ce44616d2
Charlene Coffman <Charlene.Coffman@intlfcstone.com>
2017-11-26 12:37:19 -0600

    Fixed the random poem generator!

--- layout/scripts/jquery.shared.js
+++ layout/scripts/jquery.shared.js
@@ -3,6 +3,7 @@ $("#header_placeholder").load("/includes/header.html");
 $("#footer_placeholder").load("/includes/footer.html");
 $("#copyright_placeholder").load("/includes/copyright.html");
 $("#topnav_placeholder").load("/includes/topmedianav.html");
+ShowPoem();
 
 $.ajax({
 		url: "http://dynamic.xkcd.com/api-0/jsonp/comic?callback=?",
@@ -18,8 +19,19 @@ $.ajax({
 	  });
 		  
 function ShowPoem() {
+	var randomWord = GetRandomWord();
+	var url = 'http://www.stands4.com/services/v2/poetry.php?uid=6045&tokenid=aQCgkteFXzfeLRjH&term=' + randomWord;
 	
-	httpGetAsync('http://poetrydb.org/author,title/Shakespeare;From%20fairest');
+	httpGetAsync(url, function(theJson) { 
+						var theChosenOne = PickAPoem(theJson);
+						if(theChosenOne['poembody'] != "")
+						{
+							$("#poem_title").html(theChosenOne['title']);
+							$("#author").html(theChosenOne['poem']);
+							$("#poem_body").html(theChosenOne['poembody']);
+						}
+					  }
+				);
 }
 	
 function httpGetAsync(theUrl, callback)
@@ -27,9 +39,43 @@ function httpGetAsync(theUrl, callback)
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function() { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+            callback(xmlHttp.responseXML);
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+    xmlHttp.send();
+}
+
+function GetRandomWord() {
+	
+	var arrayOfRandomWords = new Array('grass', 'love', 'stars', 'eyes', 'tango', 'eternal', 'code', 'emily', 'hairy', 'dinosaur', 'butts');
+	var randomnumber = Math.floor(Math.random() * arrayOfRandomWords.length)
+	
+	return arrayOfRandomWords[randomnumber];
+}
+
+function PickAPoem(theJson) {
+	
+	var results = theJson.getElementsByTagName("results")[0].childNodes;
+	
+	var returnPoem = new Object();
+		
+	for(i=0; i<results.length; i++)
+	{
+		var poem = results[i].childNodes;
+		
+		returnPoem.poembody = poem[2].innerHTML;
+		var poemLength = returnPoem.poembody.length;
+		
+		if(poemLength > 100 && poemLength < 600)
+		{
+			returnPoem.title = poem[0].innerHTML;
+			returnPoem.poet = poem[1].innerHTML;	
+			return returnPoem;		
+		}
+		
+	}	
+	returnPoem.poembody = "";
+	return returnPoem;
+	
 }
 	
